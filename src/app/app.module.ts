39bd5a271f9b5e39a5b5e6b5e2c63719f4a702cf
Nilupulie Kariyawasam <nilupuliekariyawasam42@gmail.com>
2018-05-04 13:04:48 +0530

    test project with brand nav and top nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
